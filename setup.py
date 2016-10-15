ac7e012a198774c42f29071d7ec6efd2507b1455
felko <baxbaxwalanuksiwe@gmail.com>
2016-10-15 19:22:49 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.4
+# coding: utf-8
+
+from distutils.core import setup
+
+setup(name='modular',
+      version='1.0',
+      description='A lightweight library for writing modular code',
+      author='baxbaxwalanuksiwe',
+      author_email='baxbaxwalanuksiwe@gmail.com',
+      url='https://github.com/felko/modular'
+)
